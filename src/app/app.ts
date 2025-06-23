e65d311ccf7670c9e9e311d399ea2e2803629e22
Guilherme <Guilhermesantos.2008@outlook.com.br>
2025-06-22 22:15:11 -0300

    feat: add Footer and PlansPrice components to app imports

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {Header} from './header/header';
 import {MainHome} from './main-home/main-home';
+import {Footer} from './footer/footer';
+import {PlansPrice} from './plans-price/plans-price';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, MainHome],
+  imports: [RouterOutlet, Header, MainHome, Footer, PlansPrice],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
