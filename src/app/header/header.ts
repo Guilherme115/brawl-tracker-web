7b764f4c20a27a0f4d67dad8814be39860117e54
Guilherme <Guilhermesantos.2008@outlook.com.br>
2025-06-23 21:28:09 -0300

    feat: add header animations, link hover effects, and user profile component

--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { UserProfileComponent } from '../user-profile/user-profile';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
+  standalone: true,
   selector: 'app-header',
-  imports: [],
+  imports: [
+    RouterLink,
+    UserProfileComponent
+  ],
   templateUrl: './header.html',
-  styleUrl: './header.css'
+  styleUrl: './header.css',
+  animations: [
+    trigger('iconAnimation', [
+      state('initial', style({
+        transform: 'scale(1)'
+      })),
+      state('clicked', style({
+        transform: 'scale(1.2)'
+      })),
+      transition('initial <=> clicked', [
+        animate('200ms ease-in-out')
+      ])
+    ])
+  ]
 })
 export class Header {
+  iconState = 'initial';
 
+  toggleIcon() {
+    this.iconState = this.iconState === 'initial' ? 'clicked' : 'initial';
+  }
 }
