d88d8c43dcef67e3f80b5564bbef68dc043383b0
Guilherme <Guilhermesantos.2008@outlook.com.br>
2025-06-23 21:27:38 -0300

    feat: add route animations and special effects for dynamic page transitions

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,47 @@
 import { Routes } from '@angular/router';
-import {Header} from './header/header';
-import {MainHome} from './main-home/main-home';
-import {DefaultLayout} from './layouts/default-layout/default-layout';
+
+import { MainHome } from './main-home/main-home';
+import { Header } from './header/header';
+import { Footer } from './footer/footer';
+import { PlansPrice } from './plans-price/plans-price';
+import { Commands } from './commands/commands';
+import { ContactUs } from './contact-us/contact-us';
+import { Loginsucess } from './loginsucess/loginsucess';
 
 export const routes: Routes = [
+  {
+    path: '',
+    component: MainHome,
+    data: { animation: 'HomePage' }
+  },
+  {
+    path: 'header',
+    component: Header,
+    data: { animation: 'HeaderPage' }
+  },
+  {
+    path: 'login-success',
+    component: Loginsucess,
+    data: { animation: 'LoginPage' }
+  },
+  {
+    path: 'footer',
+    component: Footer,
+    data: { animation: 'FooterPage' }
+  },
+  {
+    path: 'plans',
+    component: PlansPrice,
+    data: { animation: 'PlansPage' }
+  },
+  {
+    path: 'commands',
+    component: Commands,
+    data: { animation: 'CommandsPage' }
+  },
+  {
+    path: 'contact',
+    component: ContactUs,
+    data: { animation: 'ContactPage' }
+  }
 ];
