d355d3c35f75a8457cf138bf3c5e774fcc1cd725
Guilherme <Guilhermesantos.2008@outlook.com.br>
2025-08-05 20:29:08 -0300

    feat: integrate animations and random image rotation in main-home component

--- src/app/main-home/main-home.ts
+++ src/app/main-home/main-home.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { trigger, transition, style, animate } from '@angular/animations';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-main-home',
-  imports: [],
+  standalone: true,
+  imports: [TranslateModule],
   templateUrl: './main-home.html',
-  styleUrl: './main-home.css'
+  styleUrl: './main-home.css',
+  animations: [
+    trigger('fadeAnimation', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('800ms ease-in', style({ opacity: 1 }))
+      ])
+    ])
+  ]
 })
-export class MainHome {
+export class MainHome implements OnInit {
+  images = [
+    'assets/imgs/imagem_2025-06-22_144010800-removebg-preview.png',
+    'assets/imgs/imagem_2025-07-26_161954579-removebg-preview.png',
+    'assets/imgs/imagem_2025-07-26_191245751-removebg-preview.png',
 
+  ];
+  currentImageIndex = Math.floor(Math.random() * 3);
+
+  ngOnInit() {
+    setInterval(() => {
+      this.currentImageIndex = (this.currentImageIndex + 1) % this.images.length;
+    }, 25000); // troca a cada 5 segundos
+  }
 }
